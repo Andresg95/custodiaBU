89fb0d114424cd649620306703d57daa8367721c
Andrés Garcia <Mokis@Andress-MBP.home>
2020-09-27 20:07:21 +0200

    front end build

---
+++ frontend/build/precache-manifest.0c1706e7b2124daa14166124f6913713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47f5da9d45e2d8f50cd0",
+    "url": "/static/js/main.7e3deb8c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "162d44d75712a9da353a",
+    "url": "/static/css/2.ccd1bfda.chunk.css"
+  },
+  {
+    "revision": "162d44d75712a9da353a",
+    "url": "/static/js/2.d4e00e86.chunk.js"
+  },
+  {
+    "revision": "3508ec6e0c87fb0050f4a0b0e6d954f2",
+    "url": "/static/media/notAuthWall.3508ec6e.jpeg"
+  },
+  {
+    "revision": "f4fd5b0963d65c62583f7e5529090541",
+    "url": "/index.html"
+  }
+];
